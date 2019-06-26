29e5690608c920c901c97e19c5523fc00aaf780a
Md Faroque Hossain <faroque.hossain@live.com>
2019-06-27 04:41:39 +0800

    added source code

---
+++ EntityToCsvSerializer/EntityToCsvSerializer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace EntityToCsv
+{
+    public static class EntityToCsvSerializer
+    {
+        private static char _delimiter;
+        private static volatile StringBuilder _stringBuilder;
+        private static PropertyDescriptorCollection _propertyDescriptorCollection;
+        private static readonly object Locker = new object();
+
+        //Common Format and MIME Type for CSV: https://tools.ietf.org/html/rfc4180
+        private const string EscapedDoubleQuote = "\"";
+        private static readonly char[] CsvEscapedTokens = { '\'', '\"', ',', '\n', '\r' };
+
+        public static string SerializeToDelimitedText<T>(IEnumerable<T> enumerable, char delimiter = ',')
+        {
+            try
+            {
+                lock (Locker)
+                {
+                    Setup<T>(delimiter);
+                    Serialize(enumerable);
+                    return GetSerializedText();
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new ApplicationException(exception.Message, exception);
+            }
+        }
+
+        private static void Setup<T>(char delimiter)
+        {
+            _delimiter = delimiter;
+            _stringBuilder = new StringBuilder();
+            _propertyDescriptorCollection = TypeDescriptor.GetProperties(typeof(T));
+        }
+
+        private static void Serialize<T>(IEnumerable<T> records)
+        {
+            WriteHeader();
+            WriteValues(records);
+        }
+
+        private static void WriteHeader()
+        {
+            var propertyCount = 0;
+            foreach (PropertyDescriptor property in _propertyDescriptorCollection)
+            {
+                if (propertyCount++ > 0) _stringBuilder.Append(_delimiter);
+                _stringBuilder.Append(property.DisplayName);
+            }
+
+            _stringBuilder.AppendLine();
+        }
+
+        private static void WriteValues<T>(IEnumerable<T> records)
+        {
+            foreach (var item in records)
+            {
+                var propertyCount = 0;
+                foreach (PropertyDescriptor property in _propertyDescriptorCollection)
+                {
+                    if (propertyCount++ > 0) _stringBuilder.Append(_delimiter);
+                    _stringBuilder.Append(GetCsvComplianceValue(property, item));
+                }
+
+                _stringBuilder.AppendLine();
+            }
+        }
+
+        private static string GetCsvComplianceValue<T>(PropertyDescriptor property, T item)
+        {
+            var value = property.Converter?.ConvertToString(property.GetValue(item) ?? string.Empty);
+            if (value?.IndexOfAny(CsvEscapedTokens) >= 0)
+            {
+                value = EscapedDoubleQuote + value.Replace(EscapedDoubleQuote, "\"\"") + EscapedDoubleQuote;
+            }
+
+            return value;
+        }
+
+        private static string GetSerializedText()
+        {
+            return _stringBuilder.ToString();
+        }
+    }
+}
---
+++ EntityToCsvTests/EntityToCsvSerializerTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EntityToCsv;
+
+namespace EntityToCsvTests
+{
+    [TestClass]
+    public class EntityToCsvSerializerTests
+    {
+        [TestMethod]
+        public void EntityToCsvTest()
+        {
+            var actualCsvRecords  = EntityToCsvSerializer.SerializeToDelimitedText(GetTestEntityCollection(), ',');
+            var expectedCsvRecords = GetExpectedCsvRecordsForNonEmptyEntity();
+            Assert.AreEqual(actualCsvRecords, expectedCsvRecords);
+        }
+
+        [TestMethod]
+        public void EmptyEntityToCsvTest()
+        {
+            var actualCsvRecords = EntityToCsvSerializer.SerializeToDelimitedText(new List<TestPersonEntity>(), ',');
+            var expectedEmptyCsvRecord = GetExpectedCsvRecordForEmptyEntity();
+            Assert.AreEqual(actualCsvRecords, expectedEmptyCsvRecord);
+        }
+
+        private static IEnumerable<TestPersonEntity> GetTestEntityCollection()
+        {
+            return
+                new List<TestPersonEntity>
+                {
+                    new TestPersonEntity
+                    {
+                        FirstName = "Md Faroque",
+                        LastName = "Hossain",
+                        Designation = "Senior Software Engineer",
+                        DescriptionInEnglish = "I love 'programming', 'logic', solving 'problems'!",
+                        DescriptionInSwedish = "Jag älskar \"programmering\", \"logik\", lösa \"problem\"!",
+                        DecimalTestField = 2456.13,
+                        IntegerTestField = 9988
+                    },
+                    new TestPersonEntity
+                    {
+                        FirstName = "Shuvashish",
+                        LastName = "Das",
+                        Designation = "Lead Software Engineer",
+                        DescriptionInEnglish = "I love logic, solving programming challenges!",
+                        DescriptionInSwedish = "Jag älskar logik, lösning av programutmaningar!",
+                        DecimalTestField = 5687.32,
+                        IntegerTestField = 1122
+                    }
+                };
+        }
+
+        private static string GetExpectedCsvRecordsForNonEmptyEntity(char delimiter = ',')
+        {
+            return
+                $"FirstName{delimiter}LastName{delimiter}Designation{delimiter}DescriptionInEnglish{delimiter}DescriptionInSwedish{delimiter}DecimalTestField{delimiter}IntegerTestField" +
+                $"{Environment.NewLine}" +
+                $"Md Faroque{delimiter}" +
+                $"Hossain{delimiter}" +
+                $"Senior Software Engineer{delimiter}" +
+                $"\"I love 'programming', 'logic', solving 'problems'!\"{delimiter}" +
+                $"\"Jag älskar \"\"programmering\"\", \"\"logik\"\", lösa \"\"problem\"\"!\"{delimiter}" +
+                $"2456.13{delimiter}" +
+                $"9988{Environment.NewLine}" +
+                $"Shuvashish{delimiter}" +
+                $"Das{delimiter}" +
+                $"Lead Software Engineer{delimiter}" +
+                $"\"I love logic, solving programming challenges!\"{delimiter}" +
+                $"\"Jag älskar logik, lösning av programutmaningar!\"{delimiter}" +
+                $"5687.32{delimiter}" +
+                $"1122{Environment.NewLine}";
+        }
+
+        private static string GetExpectedCsvRecordForEmptyEntity(char delimiter = ',')
+        {
+            return
+                $"FirstName{delimiter}LastName{delimiter}Designation{delimiter}DescriptionInEnglish{delimiter}DescriptionInSwedish{delimiter}DecimalTestField{delimiter}IntegerTestField" +
+                $"{Environment.NewLine}";
+        }
+    }
+
+    internal class TestPersonEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Designation { get; set; }
+        public string DescriptionInEnglish { get; set; }
+        public string DescriptionInSwedish { get; set; }
+        public double DecimalTestField { get; set; }
+        public int IntegerTestField { get; set; }
+    }
+}
